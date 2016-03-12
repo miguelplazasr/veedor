5e22e601301a6c939941119965c96d9615939d35
miguelplazasr <miguelplazasr@gmail.com>
2016-03-12 12:29:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+veedor
+======
+
+A Symfony project created on March 7, 2016, 8:38 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Document\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/create")
+     *
+     * @return Response
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('Coffe');
+        $product->setPrice('19.99');
+
+        $dm = $this->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
+
+        return new Response('created product id'.$product->getId());
+    }
+}
